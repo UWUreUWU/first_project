57b8fa06f4eaa30b760dba1d56f917a0ae49476d
movavi_school <Kovalenkoaa2008@icloud.com>
2022-10-02 16:23:51 +0700

    g

---
+++ 3/main.py
@@ -0,0 +1,41 @@
+# # # # # x = ["А","Ы","В","Ф","П","Р","О",]
+# # # # # print("строка:", x)
+# # # # # print("первый элемент=, x[0]")
+# # # # # print("последний элемент =", x[-1])
+# # # # # print("перрвый 5 элементов =", x[0:5]
+# # # # x = "я сторка ,  я карта , я Милана"
+# # # # print(x[-6:])
+# # # # print(x[:8])
+# # # x = ("введите слово:")
+# # # # temp = x[-1]
+# # # # index = x.index(temp)
+# # # # print(in)
+# # # print(len(x))
+# # path = "C:/Python3/python.exe"
+# # print("Имя файла:", path2[-1:])
+# # print("расширение:", path2[-1][-3:])
+# #  print("Имя католога:", path2[1])
+# #  print("полный путь к катологу:", {path2[0]}/{})
+# #
+# # path2 = path.split("/")
+# # print(path2)
+# chapter_1 =input("глава 1:")
+# page_1 = input("Страница: ")
+#
+# chapter_2 =input("глава 2:")
+# page_2 = input("Страница: ")
+#
+# chapter_3 =input("глава 3:")
+# page_13= input("Страница: ")
+#
+#
+# print(chapter_1, page_1.rjust(15))
+# print(chapter_2, page_2.rjust(15))
+# print(chapter_3, page_3.rjust(15))
+x = "12'345'678
+## решение через. split()
+# temp = x.split("'")
+# print(temp)
+# number = temp[0] + temp[1] + temp[2]
+
+## решение через[]
