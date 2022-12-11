0af9fad99efd42148f8332ea7742f15b11ae768b
movavi_school <selleran@list.ru>
2022-12-11 16:31:06 +0700

    11 декобря

---
+++ 1/12/12.py
@@ -0,0 +1,24 @@
+# # # l = [1, 1, 3, 4]
+# # # l = set(l)
+# # # print(l)
+# #
+# # l = (3,4,5,6,7,8,9)
+# # lst = list(set)
+# # j = (8,9,10,11,12)
+# # lst = list(set)
+# # print(l+j)
+# # set(l+j)
+# #
+# # a = [2,3,4,5,64,87,9,]
+# # for i in range(2 ,len(a)):
+# # if a[i] > a[i - 1]:
+# # print(a[i], and=' ')
+#
+#
+# a = (1,2,3,4,5)
+# b = (4,5,6,7,8)
+# s = set(a) & set(b)
+#
+
+b = [1, 2, 4, 2, 6, 1, 2, 9]
+ for i in text
