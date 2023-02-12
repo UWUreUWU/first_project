39e516cee8b31625f4b8e86b9ac748eb13b04927
movavi_school <emnovitko@gmail.com>
2023-02-12 16:25:35 +0700

    12 феб

---
+++ 1/19/mani.py
@@ -0,0 +1,20 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet_ru = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+alphabet2 = alphabet[::-1]
+alphabet2 = alphabet[::-1]
+phrase=input("ведите сообщение на ангийском , а я зашифрую на Афбэт")
+phrase2 = ''
+for i in phrase:
+    if i.upper() in alphabet_ru:
+        indx = alphabet_ru.index(i)
+        bukva = alphabet_ru[indx]
+        indx = alphabet_ru.index(i)
+        bukva = alphabet_ru2[indx]
+    elif:
+    elif i not in alphabet
+    phrase2 += i
+    elif i.upper() in alphabet
+    indx = alphabet.index(i)
+    bukva = alphabet2[indx]
+    djorono = phrase2 + bukva
+print(phrase2)
