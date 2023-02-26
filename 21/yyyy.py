d74a18f21032e1de17c2b6c09e0577d2e84aba2e
smir-ant <abcsmir.ant@gmail.com>
2023-02-26 16:24:49 +0700

    26 февраля

---
+++ 21/yyyy.py
@@ -0,0 +1,70 @@
+# f = open("file.txt", "w", encoding="utf-8")
+# f.write("аб э")
+# f.close()
+#
+# try:
+# f = open("filex,txt", "r", encoding="utf-8")
+# except FileNotFoundError:
+#     print("eee")
+#
+#
+# try:
+#     x = int(input("введи числооо:"))
+#     print(10/x)
+# except ValueError:
+#     print("чеслоооооо")
+# except ZeroDivisionError:
+#     print("нет")
+# else:
+#     print("ага")
+#
+# x = input("введите имя друга:")
+# try:
+#     if x == "негр":
+#         raise NameError("еееее")
+# except NameError as eee:
+#     print("")
+#
+# def sr_ar():
+#         s += int(number)
+#         except ValueError:
+#             print("найти не чесло:",nuber)
+#         else:
+#             k += 1
+#
+#     if k == 0:
+#     return round(s/k, 2)
+#
+#
+# try:
+#     f = open("file.txt", "r", encoding="utf-8")
+#  try:
+# #     f = open("file.txt", "r", encoding="utf-8")
+# # except FileNotFoundError:
+# #     print("айла нет или неправельное название.")
+# # text = f.read()
+# # f.close()
+#     print("айла нет или неправельное название.")
+# text = f.read()
+# f.close()
+#
+#     print(sr_ar())
+#
+# try:
+#     f = open("file.txt", "r", encoding="utf-8")
+# except FileNotFoundError:
+#     print("айла нет или неправельное название.")
+# text = f.readlines()
+# print(f"строк: {len(text)}")
+#
+# full_text = " ".join(text).replace("\n", "")
+# print(full_text)
+# words = full_text.split()
+# print(f"слов: {len(words)}")
+# symbols = full_text.replace(" ", "")
+#
+#
+# print(f"символов: {len(symbols)}")
+# f.close()
+#
+
