9871dc384c7de0608c8d8e246e94a26a4d628570
smir-ant <abcsmir.ant@gmail.com>
2023-03-05 16:30:17 +0700

    5 марта

---
+++ 22/pon.py
@@ -0,0 +1,37 @@
+#
+# import json
+#
+#  f = open("file.json", "w")
+# # # f.write("True")
+# peremennaya = [1, 4.3 [0, 0], (0, 0), None, True, "РУС"]
+# json.dump(peremennaya, f, ensure_ascii=False)
+# f.close()
+#
+#
+# f = open("file.json", "r")
+# # print(f.read(()))
+# content = json.load(f)
+# print(content)
+# print(type(content))
+# f.close()
+#
+# f = open("fule.txt", "r", encoding="utf-8")
+# u = f.readlines()
+# f.close()
+# spic = {}
+# for i in u:
+#     p = i.split(": ")
+#     spic[p[0]] = p[1].rstrip()
+# print(spic)
+#
+# o = open("file.json", "w", encoding="utf-8")
+# json.dpic(spic, o, ensure)
+
+#
+# f = open("file.json", "r")
+# sod = json.load(f)
+# f.close()
+# print(sod)
+# for indx, el in enumerate(sod):
+#     if type(i) == str:
+#         print(i)
