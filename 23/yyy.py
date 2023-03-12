61b1c2a5369076d1cd89fdc9e7367b50c15fe210
movavi_school <klimnesterov8@mail.com>
2023-03-12 16:29:15 +0700

    12 марта

---
+++ 23/yyy.py
@@ -0,0 +1,59 @@
+# new*
+# class person:
+#     new*
+#     def __init__(self, imya, vozrast):
+#         self.name = imya
+#         self.age = vozrast
+#     new*
+#     def __str__(self):
+#         return f"я {self.name} и мне {self.age} лет"
+#
+#
+# cheloveek = person("спидвагон, 89") # создание обекта класа персон
+# cheloveek1 = person("джоджо, 25") # создание обекта класа персон
+#
+# print(cheloveek.name)
+# print(cheloveek1.name)
+# print(cheloveek)
+#
+# str(cheloveek)
+#
+#
+# class Klass:
+#     new*
+#     def __init__(self, imyau, familia, voz):
+#         self.name = imyau
+#         self.femiliya = familia
+#         self.vozrast = voz
+#
+#     new*
+#     def __int__(self):
+#         return f"возраст: {man.vozrast}\nимя: {man.name}\nфамилия: {man.femiliya}
+#     new*
+#     def greet(self):
+#         return f"привет, меня зовут {self.name}, мне {self.vozrast} лет"
+# man = Klass("пон", "тампоннов", 15)
+# print(man)
+# for i in range(random.randint(5, 10)):
+#     list.append(random.randint(2, 5))
+# print(list)
+#
+#
+# class Person
+#     new*
+#     def __init__(self, imyau, familia, voz):
+#         self.name = imyau
+#         self.femiliya = familia
+#         self.vozrast = voz
+#         self.grades = []
+#
+#     new*
+#     def __int__(self):
+#         return f"возраст: {man.vozrast}\nимя: {man.name}\nфамилия: {man.femiliya}
+#
+#     new*
+#     def greet(self):
+#         return f"привет, меня зовут {self.name}, мне {self.vozrast} лет"
+# man = Person("пон", "тампоннов", 15)
+#
+# chel = Person("поннн", "абадум")
