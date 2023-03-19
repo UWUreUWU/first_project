0245b676b79883e4d13417a91aff5e89cf773164
movavi_school <ScallyMilano91@gamil.com>
2023-03-19 16:32:08 +0700

    19 марта

---
+++ 24/ani.py
@@ -0,0 +1,44 @@
+# import random
+# class Human:
+#     new*
+#     def __init__(self):
+#         self.height = 993
+#         self.__money = 0.3
+#
+#         new*
+#
+#         def zdarova(self):
+#             in self.__money > 50:
+#                 return True
+#         else:
+#             return "опрошу"
+#
+#
+#     new*
+#     def __normal_height(srlf):
+#         in 100 < self.height < 200
+# class car:
+#
+#     def start(self):
+#         return "автомобиль заведён"
+#
+#
+# def stop(self):
+#     return "автомабиль заглушон"
+#
+#
+# def y(self, year):
+#     self.year = year
+#
+#
+#
+#
+# def c(self, color):
+#     self.color = colo
+#
+# import datetime
+#
+# class Overwatch:
+#
+#     def __init__(self):
+#         self.h,self.m, self.s = datetime.datetime.now().strftime("%H:%M:%S"),split
