269421e0436158c4e5c72836cb62690a67e29b88
movavi_school <alexomger@mail.ru>
2023-04-16 16:29:56 +0700

    16 апреля

---
+++ 28/gggg.py
@@ -0,0 +1,79 @@
+from tkinter import *
+# #
+# # okno = Tk()
+# # okno.geometry("200x600")
+# # tekstik = Label(okno, taxt="голубой")
+# # tekstik.pack()
+# ent = Entry(okno,
+# #             width=20)
+# # ent.pack()
+# #
+# # btn1 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn2 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn3 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn4 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn5 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn6 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# # btn7 = Button(okno,
+# #               width=22,
+# #               dg="red"
+# #               )
+# #
+# #
+# #
+# #
+# okno.mainloop()
+#
+# from tkinter import *
+#
+# def mame():
+#         message = ent1.get()
+#         ent1.delete(0, END)
+#         message1 = txt.get(0.0, END)
+#         txt.delete(0.0, END)
+#         print(message,message1 )
+# root = Tk()
+#
+# ldl1 = Label(root, text="твой адрес:")
+# ldl1.pack()
+#
+# ent1 = Entry(root, width=10)
+# ent1.pack()
+#
+# ldl2 = Label(root, text="комент:")
+# ldl2.pack()
+#
+# txt = Text(root, width=25, heigth=10)
+# txt.pack()
+#
+# btn = Button(root, text="отправить, width=15, bg=F24DF6", command=mame)
+# btn.pack()
+# d
+# root = Tk()
+#
+# lab = Label(root, text='хай')
+#
+# lab['font'] = "Arial 15 bold"
+#
+# lab.pack()
+#
+# lkm = lab.bind('<Button-1>', bold)
+# pkm =
---
+++ 28/pon.py
@@ -0,0 +1,32 @@
+# from tkinter import *
+#
+# def hell_o(event):
+#     message = ent.get()
+#     ent.delete(0, END)
+#     print("ponn")
+# root = Tk()
+# root.geometry("500x400")
+# root.title("мой первый")
+# root["dg"] = "li"
+# lab = Label(root, text="пон")
+# lab.pack()
+#
+# lab["bg"] = "pink"
+# lab["font"] = ("Arial", 20, "bold")
+# lab["height"] = 3
+#
+# btn = Button(root, text="мой первый(с, с 13:30)", bg="lightblue", font=26,#command=hell_o
+#              )
+# btn.pack()
+# btn.bind("<Button-1>",hell_o)
+# ent = Entry(root,
+#             width=10,
+#             font=20,
+#             bd=20)
+# ent.pack()
+# txt = Text(root,
+#            width=20,
+#            heigth=5,
+#            wrap="none")
+# txt.pack()
+# root.mainloop()
