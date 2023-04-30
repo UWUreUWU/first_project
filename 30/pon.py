28a69a72fbafc9a32220140d14d8b9a8e93daba9
Komarovdb <hellosser788@gmail.com>
2023-04-30 16:26:27 +0700

    30 pon

---
+++ 30/pon.py
@@ -0,0 +1,75 @@
+from tkinter import *
+root = Tk()
+root.geometry("500x500")
+
+
+# lst = ["первый","второй","пятнадцать"]
+# lstbox = Listbox(root)
+# lstbox.pack()
+#
+# for elem in lst:
+#     lstbox.insert(END, elem)
+#
+# lst = ["первый","второй","пятнадцать"]
+# lst_tk = StringVar(value=lst)
+# lstbox = Listbox(root, listvariable=lst_tk)
+# lstbox.pack()
+
+# def fu(pompu):
+#     for ind in lstbox.curselection():
+# print(lst[ind])
+#
+# lstbox.bind('<<ListboxSelect>>',fu)
+
+# btn = Button(root, text="вывести", command=fu)
+# btn.pack()
+#
+#
+# from tkinter import messagebox
+#
+# messagebox.showinfo("туда", "теперь ты шариш")
+# messagebox.showerror()
+# messagebox.showwarning()
+#
+# btn = Button(root, text="текст")
+# # btn.pack(anchor=SW, expand=True)
+# btn.pack(anchor=SW, expand=True)
+# btn.pack(f)
+#
+# btn1 = Button(root, text="текст")
+# btn1.pack(pady=50)
+# btn2 = Button(root, text="текст")
+# btn2.pack(ipady=50)
+#
+# btn1 = Button(root, text="текст1")
+# btn1.grid(column=0, row=0)
+# btn2 = Button(root, text="текст2")
+# btn2.grid(column=1, row=0)
+# btn3 = Button(root, text="текст3")
+# btn3.grid(column=2, row=0, rowspan=2, sti)
+# btn4 = Button(root, text="текст4")
+# btn4.grid(column=0, row=0, columnspar=2, sticky=EW)
+#
+# btn1 = Button(root, text="текст")
+# btn1.place(x=10, y=10)
+#
+# def fu():
+#     print("ppp")
+#
+# root.after(3000, fu)
+#
+# def fu():
+#     print("ppp")
+#
+# btn1 = Button(root, text="екст1")
+# btn1.pack()
+# btn1.bind("pen", fu)
+#
+# rb_val = IntVar()
+# print(rb_val())
+
+
+
+
+
+root.mainloop()
---
+++ 30/poptt.py
@@ -0,0 +1,51 @@
+from tkinter import *
+root = Tk()
+root.geometry("200x200")
+#
+# label1 = Label(root, text="логин:")
+# label1.grid(column=0, row=0)
+# label2 = Label(root, text="логин:")
+# label2.grid(column=0, row=1)
+#
+# entry = Entry(root,)
+# entry.grid(column=1, row=0)
+# entry2 = Entry(root,show="*")
+# entry2.grid(column=1, row=1)
+#
+# btn = Button(root,text="ачем")
+# btn.grid(column=2,)
+
+buton = Button(root, text="поймай")
+buton.place(x=10, y=10)
+buton.bind("tenter",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
